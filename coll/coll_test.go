0f2dd60e472a95a1cf932f0b1b14c7e2267ea910
Jian-Hua, He <game09116@gmail.com>
2018-08-21 18:18:54 +0800

    Add pop testing

--- coll/coll_test.go
+++ coll/coll_test.go
@@ -15,3 +15,31 @@ func TestPush(t *testing.T) {
 		}
 	}
 }
+
+func TestPop(t *testing.T) {
+	coll := Collection{1, 2}
+
+	v1, coll := Pop(coll)
+	if v1 != 2 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", v1, 2)
+	}
+	if len(coll) != 1 {
+		t.Errorf("Collection length was incorrect, get: %d, wnat: %d", len(coll), 1)
+	}
+
+	v2, coll := Pop(coll)
+	if v2 != 1 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", v2, 1)
+	}
+	if len(coll) != 0 {
+		t.Errorf("Collection length was incorrect, get: %d, wnat: %d", len(coll), 0)
+	}
+
+	v3, coll := Pop(coll)
+	if v3 != nil {
+		t.Errorf("Data was incorrect, get: %v, want: nil", v3)
+	}
+	if len(coll) != 0 {
+		t.Errorf("Collection length was incorrect, get: %d, wnat: %d", len(coll), 1)
+	}
+}
