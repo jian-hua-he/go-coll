c28c8310a30f4de9dd32b92ee1af4f4bc22c5c40
Jian-Hua, He <game09116@gmail.com>
2018-08-21 18:02:34 +0800

    Implement map method

---
+++ coll/map.go
@@ -0,0 +1,13 @@
+package coll
+
+type MapFunc func(v interface{}) interface{}
+
+func (coll *Collection) Map(f MapFunc) *Collection {
+	results := make(Collection, 0)
+	for _, v := range *coll {
+		item := f(v)
+		results = append(results, item)
+	}
+
+	return &results
+}
