74c36a2e075c394bfdb8d707cacbb926d205a629
Jian-Hua, He <game09116@gmail.com>
2018-08-22 14:27:36 +0800

    Add test shift

--- coll/coll_test.go
+++ coll/coll_test.go
@@ -43,3 +43,39 @@ func TestPop(t *testing.T) {
 		t.Errorf("Collection length was incorrect, get: %d, wnat: %d", len(coll), 1)
 	}
 }
+
+func TestShift(t *testing.T) {
+	coll := Collection{1, 2, 3}
+
+	v1, coll := Shift(coll)
+	if v1 != 1 {
+		t.Errorf("Data was incorredt, get: %d, want: %d", v1, 1)
+	}
+	if len(coll) != 2 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 2)
+	}
+
+	v2, coll := Shift(coll)
+	if v2 != 2 {
+		t.Errorf("Data was incorredt, get: %d, want: %d", v2, 2)
+	}
+	if len(coll) != 1 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 1)
+	}
+
+	v3, coll := Shift(coll)
+	if v3 != 3 {
+		t.Errorf("Data was incorredt, get: %d, want: %d", v3, 3)
+	}
+	if len(coll) != 0 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 0)
+	}
+
+	v4, coll := Shift(coll)
+	if v4 != nil {
+		t.Errorf("Data was incorredt, get: %d, want: nil", v4)
+	}
+	if len(coll) != 0 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 0)
+	}
+}
