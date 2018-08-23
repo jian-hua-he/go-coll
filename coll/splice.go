f222747d857677dac229f5f54c52849772f78d0a
Jian-Hua, He <game09116@gmail.com>
2018-08-23 13:35:03 +0800

    Add splice len

--- coll/splice.go
+++ coll/splice.go
@@ -1,5 +1,11 @@
 package coll
 
 func (coll *Collection) Splice(i int) Collection {
-	return *coll
+	arr := ToInterfaces(*coll)
+	return arr[i:]
+}
+
+func (coll *Collection) SpliceLen(i int, len int) Collection {
+	arr := ToInterfaces(*coll)
+	return arr[i : i+len]
 }
--- coll/splice_test.go
+++ coll/splice_test.go
@@ -25,3 +25,40 @@ func TestSplice(t *testing.T) {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 0)
 	}
 }
+
+func TestSpliceLen(t *testing.T) {
+	coll := Collection{1, 2, 3, 4, 5}
+
+	r1 := coll.SpliceLen(2, 2)
+	if len(coll) != 5 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 5)
+	}
+	if len(r1) != 2 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r1), 2)
+	}
+	arr1 := []interface{}(r1)
+	if arr1[0] != 3 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[0], 3)
+	}
+	if arr1[1] != 4 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[1], 4)
+	}
+
+	r2 := coll.SpliceLen(0, 3)
+	if len(coll) != 5 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 5)
+	}
+	if len(r2) != 3 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r2), 3)
+	}
+	arr2 := []interface{}(r2)
+	if arr2[0] != 1 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", arr2[0], 1)
+	}
+	if arr2[1] != 2 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", arr2[1], 2)
+	}
+	if arr2[2] != 3 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", arr2[2], 3)
+	}
+}
