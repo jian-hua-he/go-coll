4017c3d19a77f006adb2bb2d0df3e1b725f54151
Jian-Hua, He <game09116@gmail.com>
2018-08-26 11:13:26 +0800

    Add Contains implementation

--- coll/contains.go
+++ coll/contains.go
@@ -1,5 +1,14 @@
 package coll
 
-func (col *Collection) Contains(f BoolFunc) bool {
+import "fmt"
+
+func (coll *Collection) Contains(f BoolFunc) bool {
+	for _, v := range *coll {
+		if f(v) {
+			fmt.Println(v)
+			return true
+		}
+	}
+
 	return false
 }
