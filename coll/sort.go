059d8c1af5df7ac98ac6e16336adeb24b02b8e38
Jian-Hua, He <game09116@gmail.com>
2018-08-28 21:57:49 +0800

    Implement sort

--- coll/sort.go
+++ coll/sort.go
@@ -3,5 +3,18 @@ package coll
 type SortFunc func(a interface{}, b interface{}) bool
 
 func (coll *Collection) Sort(f SortFunc) {
-	return
+	l := len(*coll)
+	arr := ToInterfaces(*coll)
+
+	for j := 0; j < l-1; j++ {
+		for i := j; i < l-1; i++ {
+			if f(arr[i], arr[i+1]) {
+				temp := arr[i+1]
+				arr[i+1] = arr[i]
+				arr[i] = temp
+			}
+		}
+	}
+
+	*coll = arr
 }
