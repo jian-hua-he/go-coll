a3dbc4eee7fd179626310257bdc8a6f61fdafdcf
Jian-Hua, He <game09116@gmail.com>
2018-08-29 15:03:02 +0900

    Update reduce test

--- coll/reduce.go
+++ coll/reduce.go
@@ -2,6 +2,6 @@ package coll
 
 type ReduceFunc func(c interface{}, i interface{}) interface{}
 
-func (coll *Collection) Reduce(ReduceFunc) interface{} {
+func (coll *Collection) Reduce(f ReduceFunc, init interface{}) interface{} {
 	return nil
 }
--- coll/reduce_test.go
+++ coll/reduce_test.go
@@ -3,5 +3,13 @@ package coll
 import "testing"
 
 func TestReduce(t *testing.T) {
-	t.Error("Not implement")
+	coll := Collection{1, 2, 3, 4}
+
+	r1 := coll.Reduce(func(c interface{}, i interface{}) interface{} {
+		return c.(int) + i.(int)
+	}, 5)
+
+	if r1 != 15 {
+		t.Errorf("Data was incorredt, get: %d, want: %d", r1, 15)
+	}
 }
