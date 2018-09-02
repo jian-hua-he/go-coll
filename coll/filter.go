5361619a29b9ee6c99ac81e0cbd246811f07ddbd
Jian-Hua, He <game09116@gmail.com>
2018-09-02 13:59:03 +0800

    Add documentation for filter and map

--- coll/filter.go
+++ coll/filter.go
@@ -1,6 +1,7 @@
 package coll
 
-// Filter
+// Filter method filters the Collection using the given BoolFunc,
+// keeping only those items that pass a given truth test
 func (coll *Collection) Filter(f BoolFunc) Collection {
 	results := make(Collection, 0)
 	for _, v := range *coll {
@@ -12,7 +13,9 @@ func (coll *Collection) Filter(f BoolFunc) Collection {
 	return results
 }
 
-// FilterWithIndex
+// FilterWithIndex method is almost the same thing with Filter function.
+// The only difference is FilterWithIndex use BoolFuncWithIndex
+// to determine which item should keep
 func (coll *Collection) FilterWithIndex(f BoolFuncWithIndex) Collection {
 	results := make(Collection, 0)
 	for i, v := range *coll {
--- coll/map.go
+++ coll/map.go
@@ -1,9 +1,11 @@
 package coll
 
-// MapFunc
+// MapFunc is a function that return new interface{} with given interface{}
 type MapFunc func(v interface{}) interface{}
 
-// Map will receive a MapFunc and return a new Collection
+// Map method iterates through the Collection
+// and passes each value to the given MapFunc.
+// The MapFunc is free to modify the item and return it
 func (coll *Collection) Map(f MapFunc) Collection {
 	results := make(Collection, 0)
 	for _, v := range *coll {
