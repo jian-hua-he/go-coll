2df6ad580441e30bf75e99947e1f2d0ef4e11f49
Jian-Hua, He <game09116@gmail.com>
2018-09-08 10:56:52 +0800

    Update implementation in splice.go

--- coll/splice.go
+++ coll/splice.go
@@ -3,19 +3,26 @@ package coll
 // Splice
 func (coll *Collection) Splice(i int) Collection {
 	arr := ToInterfaces(*coll)
-	*coll = arr[:i]
-	return arr[i:]
+	r := make(Collection, 0)
+	r = append(r, arr[i:]...)
+
+	p := make(Collection, 0)
+	p = append(p, arr[:i]...)
+	*coll = p
+
+	return r
 }
 
 // SpliceLen
 func (coll *Collection) SpliceLen(i int, l int) Collection {
 	arr := ToInterfaces(*coll)
+	r := make(Collection, 0)
+	r = append(r, arr[i:i+l]...)
 
-	r := make([]interface{}, 0)
-	r = append(r, arr[:i]...)
-	r = append(r, arr[i+l:]...)
+	p := make(Collection, 0)
+	p = append(p, arr[:i]...)
+	p = append(p, arr[i+l:]...)
+	*coll = p
 
-	*coll = r
-
-	return arr[i : i+l]
+	return r
 }
