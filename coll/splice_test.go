d359b0ea1f523fca8ff5bca77dc0c0461c130156
Jian-Hua, He <game09116@gmail.com>
2018-09-08 11:54:02 +0800

    Update test

--- coll/splice_test.go
+++ coll/splice_test.go
@@ -9,23 +9,28 @@ func TestSplice(t *testing.T) {
 	if len(r1) != 3 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r1), 3)
 	}
+	if cap(r1) != 3 {
+		t.Errorf("Collection capacity was incorrect, get: %d, want: %d", cap(r1), 3)
+	}
 	if len(coll) != 2 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 2)
 	}
-	arr1 := []interface{}(r1)
-	if arr1[0] != 3 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[0], 3)
+	if r1[0] != 3 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r1[0], 3)
 	}
-	if arr1[1] != 4 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[1], 4)
+	if r1[1] != 4 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r1[1], 4)
 	}
-	if arr1[2] != 5 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[2], 5)
+	if r1[2] != 5 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r1[2], 5)
 	}
 
 	r2 := coll.Splice(0)
 	if len(r2) != 2 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r2), 2)
+	}
+	if cap(r2) != 2 {
+		t.Errorf("Collection capacity was incoorect, get: %d, want: %d", cap(r2), 2)
 	}
 	if len(coll) != 0 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 0)
@@ -40,27 +45,31 @@ func TestSpliceLen(t *testing.T) {
 	if len(r1) != 2 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r1), 2)
 	}
-	arr1 := []interface{}(r1)
-	if arr1[0] != 3 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[0], 3)
+	if cap(r1) != 2 {
+		t.Errorf("Collection capacity was incorrect, get: %d, want: %d", cap(r1), 2)
 	}
-	if arr1[1] != 4 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr1[1], 4)
+	if r1[0] != 3 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r1[0], 3)
+	}
+	if r1[1] != 4 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r1[1], 4)
 	}
 
 	// Except coll1 = Collection{1, 2, 5}
 	if len(coll) != 3 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 3)
 	}
-	coll1 := []interface{}(coll)
-	if coll1[0] != 1 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", coll1[0], 1)
+	if cap(coll) != 3 {
+		t.Errorf("Collection capacity was incorrect, get: %d, want: %d", cap(coll), 3)
 	}
-	if coll1[1] != 2 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", coll1[0], 2)
+	if coll[0] != 1 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", coll[0], 1)
 	}
-	if coll1[2] != 5 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", coll1[0], 5)
+	if coll[1] != 2 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", coll[0], 2)
+	}
+	if coll[2] != 5 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", coll[0], 5)
 	}
 
 	// Except r2 = Collection{1}
@@ -68,20 +77,24 @@ func TestSpliceLen(t *testing.T) {
 	if len(r2) != 1 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(r2), 1)
 	}
-	arr2 := []interface{}(r2)
-	if arr2[0] != 1 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", arr2[0], 1)
+	if cap(r2) != 1 {
+		t.Errorf("Collection length was incorrect, get: %d, want: %d", cap(r2), 1)
+	}
+	if r2[0] != 1 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", r2[0], 1)
 	}
 
 	// Except coll = Collection{2, 5}
 	if len(coll) != 2 {
 		t.Errorf("Collection length was incorrect, get: %d, want: %d", len(coll), 2)
 	}
-	coll2 := []interface{}(coll)
-	if coll2[0] != 2 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", coll2[0], 2)
+	if cap(coll) != 2 {
+		t.Errorf("Collection capacity was incorrect, get: %d, want: %d", cap(coll), 2)
 	}
-	if coll2[1] != 5 {
-		t.Errorf("Data was incorrect, get: %d, want: %d", coll2[1], 5)
+	if coll[0] != 2 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", coll[0], 2)
+	}
+	if coll[1] != 5 {
+		t.Errorf("Data was incorrect, get: %d, want: %d", coll[1], 5)
 	}
 }
